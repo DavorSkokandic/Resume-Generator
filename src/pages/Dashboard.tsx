03a425e076f69044814aaeb5694ec77118db1cc7
Davor Skokandić <davor.skokoandic46@gmail.com>
2025-04-22 17:04:32 +0200

    Created basic page setup

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dashboard(){
+    return(
+        <div className='h-screen flex items-center justify-center bg-gray-100 text-2xl font-bold text-blue-600'>
+            Dashboard Page
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(){
+    return(
+        <div className='h-screen flex items-center justify-center bg-gray-100 text-2xl font-bold text-blue-600'>
+            Home Page
+        </div>
+    )
+}
+
+export default Home;
