22e13e00af1850d36119e44f0bc594b21dc276ac
DavorSkokandic <davor.skokandic46@gmail.com>
2025-05-07 17:57:54 +0200

    Fixed minor app.tsx problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from './routes';
-import Header from './components/Header';
 import "./output.css";
 import { ResumeProvider } from './context/ResumeContext';
 import AppContent from './AppContext';
